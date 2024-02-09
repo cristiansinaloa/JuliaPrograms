caf098dea27e3f6932c6b6c7a3b2c273b7ef2488
cristiansinaloa <cristiansinaloa@gmail.com>
2024-02-09 01:11:55 -0800

    Fixed bug not making the Potentials Work

--- Any1DPot.jl
+++ Any1DPot.jl
@@ -1,4 +1,5 @@
 #!/usr/bin/env julia
+cd(@__DIR__)
 using Pkg
 Pkg.activate(".")
 Pkg.add(Pkg.PackageSpec(name="Plots",version="1.39.0"))
@@ -10,20 +11,22 @@ L=1 #Length of the box from 0 to L
 dx = 0.001 #with of the spacing 
 N = round(Int,(L-0)/dx) # num intervals = b-a/dx 1000 intvls
 
-x = range(0,L,length=N+1) # Since we are including 0 we need N+1 points
+xpts = range(0,L,length=N+1) # Since we are including 0 we need N+1 points
+x = xpts[2:end-1]
 
 function V(x)
-    return 0*x #Infinite square well potential
-    # return  10*(x .- 0.5) .^ 2 # Harmonic Potential Centered at x=0.5
-    # return 0.5*exp.(-0.5*((x .- 0.7)/(.01)) .^ 2) / sqrt((0.1)^2) # Gaussian Potential at x=0.75
+    # return 0*x #Infinite square well potential
+    return  1000*(x .- 0.5) .^ 2 # Harmonic Potential Centered at x=0.5
+    # return 1000*exp.(-0.5*((x .- 0.7)/(.01)) .^ 2) / sqrt((0.1)^2) # Gaussian Potential at x=0.7
 end
 
 #create diags
-d = 2*ones(N-1) + V(x)[2:end-1]
+d = 2*ones(N-1) 
 e = -ones(N-2)
+k = (1/(2*dx^2)) 
 
 #Create the Tridiagonal Sparse Hamiltonian
-H = (1/(2*dx^2))*SymTridiagonal(d,e)
+H = k*SymTridiagonal(d,e) + Diagonal(V(x))
 
 w,Psi = eigen(H)
 
@@ -40,13 +43,11 @@ plot!(xlabel="x",xguidefontsize=24,xtickfontsize=24)
 plot!(ylabel="ψₙ(x)",yguidefontsize=24,ytickfontsize=24)
 #plot!(xlims=(0,1))
 #plot!(ylims=(-5,5))
-hline!(0,color=:black)
-vline!(0,color=:black)
-plot!(range(dx,1-dx,step=dx),Psi[:,1],label="Ψ₁(x)",linecolor=:blue,linewidth=4)
-#plot!(range(dx,1-dx,step=dx),(sqrt(2)*sin.(pi.*x[2:end-1])),label="Analytic ψ₁(x)",linecolor=:black,linewidth=4)
-plot!(range(dx,1-dx,step=dx),Psi[:,2],label="Ψ₂(x)",linewidth=4,linecolor=:orange)
-plot!(range(dx,1-dx,step=dx),Psi[:,3],label="Ψ₃(x)",linewidth=4,linecolor=:green)
-plot!(range(dx,1-dx,step=dx),V(x)[2:end-1],linecolor=:red,linestyle=:dash,linewidth=4,label="Potential") #Plot of the potential
+plot!(x,Psi[:,1],label="Ψ₁(x)",linecolor=:blue,linewidth=4)
+#plot!(x,(sqrt(2)*sin.(pi.*x[2:end-1])),label="Analytic ψ₁(x)",linecolor=:black,linewidth=4)
+plot!(x,Psi[:,2],label="Ψ₂(x)",linewidth=4,linecolor=:orange)
+plot!(x,Psi[:,3],label="Ψ₃(x)",linewidth=4,linecolor=:green)
+#plot!(x,V(x)[2:end-1],linecolor=:red,linestyle=:dash,linewidth=4,label="Potential") #Plot of the potential
 plot!(legend=true,legendfontsize=14,gridlinewidth=2)
 display(plot!())
 readline()
